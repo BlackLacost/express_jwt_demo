71c78964b26efa781a3d896db46c5d169e0995f4
Ilya Lisin <blacklacost@gmail.com>
2020-01-24 13:48:34 +0300

    add users router

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
+require('dotenv').config();
 const express = require('express');
 
+const apiRouter = require('./api/api-router');
+
 const app = express();
+const apiRoot = process.env.API_ROOT;
 
 app.get('/', (req, res) => res.send('API for JWT testing'));
+
+app.use(`${apiRoot}`, apiRouter);
 
 module.exports = app;
